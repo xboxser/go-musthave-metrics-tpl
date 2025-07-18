91a7c742e506d1881a2a3980441d3c0aa19e4317
xboxser <xboxser74@yandex.ru>
2025-07-18 22:12:23 +0500

    iter7 add debug

--- internal/agent/sender/sender.go
+++ internal/agent/sender/sender.go
@@ -4,17 +4,29 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 type Sender struct {
 	baseURL *string
 	client  *http.Client
+	sugar   zap.SugaredLogger
 }
 
 func NewSender(baseURL *string) *Sender {
+	var sugar zap.SugaredLogger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar = *logger.Sugar()
 	return &Sender{
 		baseURL: baseURL,
 		client:  &http.Client{},
+		sugar:   sugar,
 	}
 }
 
@@ -30,11 +42,19 @@ func (s *Sender) SendRequest(json []byte) error {
 
 	response, err := s.client.Do(req)
 	if err != nil {
+		s.sugar.Debugln(
+			"uri", url,
+			"json", string(json),
+		)
 		return fmt.Errorf("failed request: %v", err)
 	}
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
 		return fmt.Errorf("unexpected status: %s, %s", response.Status, url)
 	}
+	s.sugar.Infoln(
+		"uri", url,
+		"json", string(json),
+	)
 	return nil
 }

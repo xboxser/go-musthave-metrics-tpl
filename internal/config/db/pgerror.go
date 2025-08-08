69877a4cf557efd0fea0b9ba0c21c805f6a18e73
xboxser <xboxser74@yandex.ru>
2025-08-08 21:32:22 +0500

    iter13 add retry

---
+++ internal/config/db/pgerror.go
@@ -0,0 +1,57 @@
+package db
+
+import (
+	"errors"
+
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+// ErrorClassification тип для классификации ошибок
+type PGErrorClassification int
+
+const (
+	// NonRetriable - операцию не следует повторять
+	NonRetriable PGErrorClassification = iota
+
+	// Retriable - операцию можно повторить
+	Retriable
+)
+
+// PostgresErrorClassifier классификатор ошибок PostgreSQL
+type PostgresErrorClassifier struct{}
+
+func NewPostgresErrorClassifier() *PostgresErrorClassifier {
+	return &PostgresErrorClassifier{}
+}
+
+// Classify классифицирует ошибку и возвращает PGErrorClassification
+func (c *PostgresErrorClassifier) Classify(err error) PGErrorClassification {
+	if err == nil {
+		return NonRetriable
+	}
+
+	// Проверяем и конвертируем в pgconn.PgError, если это возможно
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return ClassifyPgError(pgErr)
+	}
+
+	// По умолчанию считаем ошибку неповторяемой
+	return NonRetriable
+}
+
+func ClassifyPgError(pgErr *pgconn.PgError) PGErrorClassification {
+	// Коды ошибок PostgreSQL: https://www.postgresql.org/docs/current/errcodes-appendix.html
+
+	switch pgErr.Code {
+	// Класс 08 - Ошибки соединения
+	case pgerrcode.ConnectionException,
+		pgerrcode.ConnectionDoesNotExist,
+		pgerrcode.ConnectionFailure:
+		return Retriable
+	}
+
+	// По умолчанию считаем ошибку неповторяемой
+	return NonRetriable
+}

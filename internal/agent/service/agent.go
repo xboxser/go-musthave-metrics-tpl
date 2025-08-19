6a65769ae9a5a6801e6377acec51f926e190da37
xboxser <xboxser74@yandex.ru>
2025-08-19 22:44:30 +0500

    iter15 вынес сбор метрик отдельно

--- internal/agent/service/agent.go
+++ internal/agent/service/agent.go
@@ -5,15 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"math/rand/v2"
 	agentModel "metrics/internal/agent/model"
 	"metrics/internal/agent/sender"
 	models "metrics/internal/model"
 	"runtime"
-	"strconv"
-
-	"github.com/shirou/gopsutil/v4/cpu"
-	"github.com/shirou/gopsutil/v4/mem"
 )
 
 type AgentService struct {
@@ -185,29 +180,9 @@ func generatorGaugeGopsutilMetrics() chan agentModel.ChanGauge {
 	outMetrics := make(chan agentModel.ChanGauge)
 	go func() {
 		defer close(outMetrics)
-		mem, err := mem.VirtualMemory()
+		err := sendGaugeGopsutil(outMetrics)
 		if err != nil {
-			log.Println("Error:", err)
-			return
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "TotalMemory",
-			Value: mem.Total,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "FreeMemory",
-			Value: mem.Free,
-		}
-		cpuPercent, err := cpu.Percent(0, true)
-		if err != nil {
-			log.Printf("Ошибка при получении загрузки CPU: %v", err)
-			return
-		}
-		for i, percent := range cpuPercent {
-			outMetrics <- agentModel.ChanGauge{
-				Name:  "CPUutilization" + strconv.Itoa(i),
-				Value: percent,
-			}
+			log.Printf("Ошибка метрик: %v", err)
 		}
 
 	}()
@@ -221,118 +196,8 @@ func generatorGaugeMemoryMetrics() chan agentModel.ChanGauge {
 		defer close(outMetrics)
 		var mem runtime.MemStats
 		runtime.ReadMemStats(&mem)
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "Alloc",
-			Value: mem.Alloc,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "BuckHashSys",
-			Value: mem.BuckHashSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "Frees",
-			Value: mem.Frees,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "GCCPUFraction",
-			Value: mem.GCCPUFraction,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "GCSys",
-			Value: mem.GCSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "HeapAlloc",
-			Value: mem.HeapAlloc,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "HeapIdle",
-			Value: mem.HeapIdle,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "HeapInuse",
-			Value: mem.HeapInuse,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "HeapObjects",
-			Value: mem.HeapObjects,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "HeapReleased",
-			Value: mem.HeapReleased,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "HeapSys",
-			Value: mem.HeapSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "LastGC",
-			Value: mem.LastGC,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "Lookups",
-			Value: mem.Lookups,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "MCacheInuse",
-			Value: mem.MCacheInuse,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "MCacheSys",
-			Value: mem.MCacheSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "MSpanInuse",
-			Value: mem.MSpanInuse,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "MSpanSys",
-			Value: mem.MSpanSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "Mallocs",
-			Value: mem.Mallocs,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "NextGC",
-			Value: mem.NextGC,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "OtherSys",
-			Value: mem.OtherSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "PauseTotalNs",
-			Value: mem.PauseTotalNs,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "StackInuse",
-			Value: mem.StackInuse,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "StackSys",
-			Value: mem.StackSys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "Sys",
-			Value: mem.Sys,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "TotalAlloc",
-			Value: mem.TotalAlloc,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "NumForcedGC",
-			Value: mem.NumForcedGC,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "NumGC",
-			Value: mem.NumGC,
-		}
-		outMetrics <- agentModel.ChanGauge{
-			Name:  "RandomValue",
-			Value: rand.Float64(),
-		}
+		sendMemStats(outMetrics, &mem)
+		sendRandomValue(outMetrics)
 	}()
 	return outMetrics
 }
